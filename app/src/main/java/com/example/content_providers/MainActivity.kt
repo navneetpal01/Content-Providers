8ad41cccd5a0c4c3c16085b0147fa4d708ea105c
navneet <navneetpalgithub@gmail.com>
2024-05-24 14:49:50 +0530

    Saturn

--- app/src/main/java/com/example/content_providers/MainActivity.kt
+++ app/src/main/java/com/example/content_providers/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.example.content_providers
 
+import android.Manifest
 import android.content.ContentUris
 import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import coil.compose.AsyncImage
 import com.example.content_providers.ui.theme.ContentProvidersTheme
 import java.util.Calendar
 
@@ -20,6 +36,7 @@ class MainActivity : ComponentActivity() {
 //    InputStream: Used to read data from a source like a file, network, or keyboard.
 //    OutputStream: Used to write data to a destination like a file, network, or display.
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.light(
@@ -28,6 +45,19 @@ class MainActivity : ComponentActivity() {
             )
         )
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES ),
+                0
+            )
+        }else{
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
+        }
         //Specific columns we want to have from the each image
         val projection = arrayOf(
             MediaStore.Images.Media._ID,
@@ -64,12 +94,34 @@ class MainActivity : ComponentActivity() {
                 )
                 images.add(Image(id,name,uri))
             }
+            Log.d("pokemon", "Let's verify $images")
             viewModel.updateImages(images)
 
         }
         setContent {
             ContentProvidersTheme {
 
+
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ){
+                    items(viewModel.images){image ->
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ){
+                            AsyncImage(
+                                model = image.uri,
+                                contentDescription = null
+                            )
+                            Text(text = image.name)
+
+                        }
+
+                    }
+                }
             }
         }
     }
